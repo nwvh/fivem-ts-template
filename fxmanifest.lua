35e087f30633fccb7f172a5fb83fb8ccfe48d74c
wx <76164598+nwvh@users.noreply.github.com>
2024-03-14 22:32:11 +0100

    feat: upload files

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+author 'wx / woox'
+version '1.0.0'
+description 'Typescript "boilerplate" for FiveM'
+game 'gta5'
+
+client_scripts {
+	'build/client/client.js'
+}
+
+server_scripts {
+	'build/server/server.js'
+}
